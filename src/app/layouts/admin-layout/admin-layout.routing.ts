cec224cb85a6691ffeee7379737b122e1e237bae
Your Name <you@example.com>
2023-03-18 01:02:33 +0700

    donedone

--- src/app/layouts/admin-layout/admin-layout.routing.ts
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -3,10 +3,12 @@ import { Routes } from '@angular/router';
 import { UserProfileComponent } from '../../pages/user-profile/user-profile.component';
 import { TablesComponent } from '../../pages/tables/tables.component';
 import { FileInfoComponent } from 'src/app/pages/tables/file-info/file-info.component';
+import {ConstructionComponent} from "../../pages/construction/construction.component";
 
 export const AdminLayoutRoutes: Routes = [
     { path: 'create',   component: UserProfileComponent },
     { path: 'tables',         component: TablesComponent },
     { path: 'file/:fileName',         component: FileInfoComponent },
+    {path:'construction', component: ConstructionComponent},
 
 ];
